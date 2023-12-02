a565afa049a1bbdd17a9077c0f5bb50d92cccdb2
Your Name <you@example.com>
2023-12-02 23:29:42 +0530

    first commit ever

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+function App() {
+const title = 'Youtube Channel'
+const body = 'This is my Youtube channel.'
+const comments = [
+ { id: 1, text: 'Comment one'},
+ { id: 2, text: 'Comment two'},
+ { id: 3, text: 'Comment three'},
+
+
+]
+  return(
+<div className='container'>
+  <h1>
+   {title.toUpperCase()}
+  </h1>
+  <p>
+    {body}
+  </p>
+<div className='comments'>
+  <h3>Comments ({comments.length})</h3>
+  <ul>
+    {comments.map((comment, index) => (
+      <li key={index}>{comment.text}</li>
+    ))}
+  </ul>
+</div>
+</div>
+  )
+  
+}
+
+
+export default App
